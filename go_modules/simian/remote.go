0281e4ed42eda79a634b24ae54ea7f66d0fdd1df
Michael Heilmann <mheilman@ist.ucf.edu>
2015-04-02 13:15:50 -0400

    Added json config file.  Simian module tests simiangrid connectivity on initialization

--- go_modules/simian/remote.go
+++ go_modules/simian/remote.go
@@ -1,5 +1,11 @@
 package simian
 
+import (
+  "net/http"
+  "fmt"
+  "io/ioutil"
+)
+
 type SimianResponse struct {
 }
 
@@ -9,14 +15,49 @@ type SimianRequest struct {
 
 type simianConnector struct {
   requests chan SimianRequest
+  url string
+}
+
+type errorString struct {
+    s string
+}
+func (es *errorString) Error() string {
+  return es.s
 }
 
 var simianInstance *simianConnector = nil
 
-func Instance() *simianConnector {
+func Instance() (*simianConnector, error) {
   if simianInstance == nil {
-    simianInstance = new(simianConnector)
+    return nil, &errorString{"simian has not been initialized"}
   }
-  return simianInstance
+  return simianInstance, nil
 }
 
+func Initialize(simianUrl string) (error) {
+  if simianInstance == nil {
+    simianInstance = &simianConnector{url: simianUrl}
+  } else {
+    return &errorString{"simian has already been initialized"}
+  }
+  //Test a connection from simianInstance to
+  url := fmt.Sprintf("http://%v/Grid/", simianInstance.url)
+  fmt.Println("simian testing on ", url)
+  resp, err := http.Get(url)
+  if err != nil {
+    return err
+  }
+  
+  body, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    return err
+  }
+  
+  result := string(body)
+  
+  if result == "SimianGrid" {
+    fmt.Println("simian initialized")
+    return nil
+  }
+  return &errorString{fmt.Sprintf("Received %s instead of SimianGrid from Simian /Grid/ path")}
+}
--- mgmServer.go
+++ mgmServer.go
@@ -2,32 +2,56 @@ package main
 
 import (
   "./go_modules/mgm"
+  "./go_modules/simian"
   "fmt"
   "net/http"
   "log"
+  "os"
+  "encoding/json"
 )
 
+type Configuration struct {
+  SimianUrl string
+}
+
 const (
-    LINK_HOST = "127.0.0.1"
-    LINK_PORT = "8000"
-    CLIENT_PORT = "8080"
+  LINK_HOST = "127.0.0.1"
+  LINK_PORT = "8000"
+  CLIENT_PORT = "8080"
 )
 
 func main() {
-    fmt.Println("running")
+  fmt.Println("Reading configuration file")
+  file, _ := os.Open("conf.json")
+  decoder := json.NewDecoder(file)
+  config := Configuration{}
+  err := decoder.Decode(&config)
+  if err != nil {
+    fmt.Println("Error readig config file: ", err)
+    return
+  }
+  
+  fmt.Println("Initializing Simiangrid Connection")
+  err = simian.Initialize(config.SimianUrl)
+  if err != nil {
+    fmt.Println("Error initializing simiangrid: ", err)
+    return
+  }
+  
+  fmt.Println("running")
     
-    regionMgr := mgm.ClientManager{}
+  regionMgr := mgm.ClientManager{}
     
-    // listen for opensim connections
-    opensim := mgm.OpenSimListener{Host:LINK_HOST, Port:LINK_PORT}
-    go opensim.Listen()
-    
-    // listen for client connections
-    fs := http.FileServer(http.Dir("/var/www/html/mgm"))
-    http.Handle("/", fs)
-    http.Handle("/ws", mgm.ClientHandler{regionMgr})
-    fmt.Println("Listening for clients on 127.0.0.1:" + CLIENT_PORT)
-    if err := http.ListenAndServe(":" + CLIENT_PORT, nil); err != nil {
-        log.Fatal("ListenAndServe:", err)
-    }
+  // listen for opensim connections
+  opensim := mgm.OpenSimListener{Host:LINK_HOST, Port:LINK_PORT}
+  go opensim.Listen()
+  
+  // listen for client connections
+  fs := http.FileServer(http.Dir("/var/www/html/mgm"))
+  http.Handle("/", fs)
+  http.Handle("/ws", mgm.ClientHandler{regionMgr})
+  fmt.Println("Listening for clients on 127.0.0.1:" + CLIENT_PORT)
+  if err := http.ListenAndServe(":" + CLIENT_PORT, nil); err != nil {
+    log.Fatal("ListenAndServe:", err)
+  }
 }
