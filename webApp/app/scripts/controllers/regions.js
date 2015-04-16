b6efd411a159d2a55938af13cf9b3fa189f3f39d
Michael Heilmann <mheilman@ist.ucf.edu>
2015-04-16 10:25:21 -0400

    regions received on the client side, and displayed by estate->regionlist

--- webApp/app/scripts/controllers/regions.js
+++ webApp/app/scripts/controllers/regions.js
@@ -8,10 +8,30 @@
  * Controller of the mgmApp
  */
 angular.module('mgmApp')
-  .controller('RegionsCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('RegionsCtrl', function ($scope, mgm) {
+
+    var regions = {}
+
+    $scope.estates = {}
+
+    for (var uuid in mgm.regions) {
+      estateifyRegion(mgm.regions[uuid]);
+    }
+
+    $scope.$on("RegionChange", estateifyRegion);
+
+    function estateifyRegion(region) {
+      if (region.UUID in regions) {
+        //this is an update to an existing region
+        angular.copy(region, regions[region.UUID]);
+      } else {
+        //this is a new region
+        regions[region.UUID] = region;
+        if (!(region.EstateName in $scope.estates)) {
+          $scope.estates[region.EstateName] = [];
+        }
+        $scope.estates[region.EstateName].push(region)
+      }
+    }
+
   });
