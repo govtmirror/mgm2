618cb25800c766c5db51b09f92bf22b6a2c71695
Michael Heilmann <mheilman@ist.ucf.edu>
2015-05-22 10:08:09 -0400

    Refined and corrected fan-out subscription object

--- core/subscribe.go
+++ core/subscribe.go
@@ -1,36 +1,56 @@
 package core
 
-type subscription struct {
+type subscription interface {
+	GetReceive() <-chan UserObject
+	Unsubscribe()
+}
+
+type sub struct {
 	pipe chan UserObject
-	del  chan chan UserObject
+	del  chan sub
 }
 
-func (s subscription) unsubscribe() {
-	s.del <- s.pipe
+func (s sub) Unsubscribe() {
+	s.del <- s
 }
 
-type subscriptionManager struct {
-	newSubscriptions chan chan UserObject
-	unsubscribe      chan chan UserObject
+func (s sub) GetReceive() <-chan UserObject {
+	return s.pipe
+}
+
+type subscriptionManager interface {
+	Subscribe() subscription
+	Broadcast(UserObject)
+}
+
+type subMgr struct {
+	newSubscriptions chan sub
+	unsubscribe      chan sub
 	broadcast        chan UserObject
 }
 
 func newSubscriptionManager() subscriptionManager {
-	sm := subscriptionManager{}
-	sm.newSubscriptions = make(chan chan UserObject, 8)
-	sm.unsubscribe = make(chan chan UserObject, 8)
+	sm := subMgr{}
+	sm.newSubscriptions = make(chan sub, 8)
+	sm.unsubscribe = make(chan sub, 8)
+	sm.broadcast = make(chan UserObject, 32)
 	go sm.process()
 	return sm
 }
 
-func (sm subscriptionManager) Subscribe() subscription {
+func (sm subMgr) Subscribe() subscription {
 	ch := make(chan UserObject, 8)
-	sm.newSubscriptions <- ch
-	return subscription{ch, sm.unsubscribe}
+	s := sub{ch, sm.unsubscribe}
+	sm.newSubscriptions <- s
+	return s
 }
 
-func (sm subscriptionManager) process() {
-	var subscribers []chan UserObject
+func (sm subMgr) Broadcast(msg UserObject) {
+	sm.broadcast <- msg
+}
+
+func (sm subMgr) process() {
+	var subscribers []sub
 	for {
 		select {
 		case s := <-sm.newSubscriptions:
@@ -40,14 +60,13 @@ func (sm subscriptionManager) process() {
 				if sub == s {
 					subscribers[i] = subscribers[len(subscribers)-1]
 					subscribers = subscribers[:len(subscribers)-1]
-					break
 				}
 			}
 		case ob := <-sm.broadcast:
-			for _, sub := range subscribers {
-				go func(ch chan<- UserObject, ob UserObject) {
-					ch <- ob
-				}(sub, ob)
+			for _, s := range subscribers {
+				go func(s sub, ob UserObject) {
+					s.pipe <- ob
+				}(s, ob)
 			}
 		}
 	}
