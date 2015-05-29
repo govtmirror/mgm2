f945a0f84217daf6455ed98d11de1ad6e5e72742
Michael Heilmann <mheilman@ist.ucf.edu>
2015-05-29 09:03:59 -0400

    database object is how just a connection source that can be leveraged or extended, and does not offer all functionality its-self.

--- core/database/database.go
+++ core/database/database.go
@@ -1,12 +1,11 @@
-package mysql
+package database
 
 import (
 	"database/sql"
 	"fmt"
 
 	"github.com/m-o-s-e-s/mgm/core"
-	"github.com/m-o-s-e-s/mgm/mgm"
-	"github.com/satori/go.uuid"
+
 	// load mysql driver
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -27,37 +26,7 @@ type db struct {
 type Database interface {
 	TestConnection() error
 
-	GetDefaultConfigs() ([]mgm.ConfigOption, error)
-	GetConfigs(regionID uuid.UUID) ([]mgm.ConfigOption, error)
-
-	GetHosts() ([]mgm.Host, error)
-	GetHostByAddress(address string) (mgm.Host, error)
-	PlaceHostOffline(id uint) (mgm.Host, error)
-	PlaceHostOnline(id uint) (mgm.Host, error)
-
-	GetEstates() ([]mgm.Estate, error)
-
-	GetRegions() ([]mgm.Region, error)
-	GetRegionsOnHost(host mgm.Host) ([]mgm.Region, error)
-	GetRegionsForUser(guid uuid.UUID) ([]mgm.Region, error)
-	GetRegionByID(id uuid.UUID) (mgm.Region, error)
-
-	GetJobsForUser(userID uuid.UUID) ([]mgm.Job, error)
-	GetJobByID(id int) (mgm.Job, error)
-	UpdateJob(job mgm.Job) error
-	DeleteJob(job mgm.Job) error
-	CreateJob(taskType string, userID uuid.UUID, data string) (mgm.Job, error)
-	CreateLoadIarJob(owner uuid.UUID, inventoryPath string) (mgm.Job, error)
-
-	GetPendingUsers() ([]mgm.PendingUser, error)
-	AddPendingUser(name string, email string, template string, password string, summary string) error
-
-	ValidatePasswordToken(userID uuid.UUID, token uuid.UUID) (bool, error)
-	ScrubPasswordToken(token uuid.UUID) error
-	ExpirePasswordTokens() error
-	IsEmailUnique(email string) (bool, error)
-	IsNameUnique(name string) (bool, error)
-	CreatePasswordResetToken(userID uuid.UUID) (uuid.UUID, error)
+	GetConnection() (*sql.DB, error)
 }
 
 // NewDatabase is a Database constructor
@@ -65,8 +34,8 @@ func NewDatabase(username string, password string, database string, host string,
 	return db{username, password, database, host, log}
 }
 
-func (d db) TestConnection() error {
-	con, err := sql.Open("mysql", fmt.Sprintf("%v:%v@tcp(%v:3306)/%v", d.user, d.password, d.host, d.database))
+func (db db) TestConnection() error {
+	con, err := db.GetConnection()
 	if err != nil {
 		return err
 	}
@@ -74,4 +43,12 @@ func (d db) TestConnection() error {
 
 	err = con.Ping()
 	return err
+}
+
+func (db db) GetConnection() (*sql.DB, error) {
+	con, err := sql.Open("mysql", fmt.Sprintf("%v:%v@tcp(%v:3306)/%v?parseTime=true", db.user, db.password, db.host, db.database))
+	if err != nil {
+		return nil, err
+	}
+	return con, nil
 }
