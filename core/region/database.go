dd6698e46b9f9d5b491c668a218430e740fedbe0
Michael Heilmann <mheilman@ist.ucf.edu>
2015-05-29 10:06:27 -0400

    Moved region functions to region module

--- core/region/database.go
+++ core/region/database.go
@@ -1,6 +1,8 @@
 package region
 
 import (
+	"errors"
+
 	"github.com/m-o-s-e-s/mgm/core/database"
 	"github.com/m-o-s-e-s/mgm/mgm"
 	"github.com/satori/go.uuid"
@@ -8,6 +10,175 @@ import (
 
 type regionDatabase struct {
 	mysql database.Database
+}
+
+// GetRegionsForUser retrieves region records for a user where the user owns the estate they are in, or is a manager for said estate
+func (db regionDatabase) GetRegionsForUser(guid uuid.UUID) ([]mgm.Region, error) {
+	con, err := db.mysql.GetConnection()
+	if err != nil {
+		return nil, err
+	}
+	defer con.Close()
+
+	var regions []mgm.Region
+
+	rows, err := con.Query(
+		"Select uuid, name, size, httpPort, consolePort, consoleUname, consolePass, locX, locY, externalAddress, slaveAddress, isRunning, EstateName from regions, estate_map, estate_settings " +
+			"where estate_map.RegionID = regions.uuid AND estate_map.EstateID = estate_settings.EstateID AND uuid in " +
+			"(SELECT RegionID FROM estate_map WHERE " +
+			"EstateID in (SELECT EstateID FROM estate_settings WHERE EstateOwner=\"" + guid.String() + "\") OR " +
+			"EstateID in (SELECT EstateID from estate_managers WHERE uuid=\"" + guid.String() + "\"))")
+	defer rows.Close()
+	if err != nil {
+		if err.Error() == "sql: no rows in result set" {
+			return regions, nil
+		}
+		return nil, err
+	}
+
+	for rows.Next() {
+		r := mgm.Region{}
+		err = rows.Scan(
+			&r.UUID,
+			&r.Name,
+			&r.Size,
+			&r.HTTPPort,
+			&r.ConsolePort,
+			&r.ConsoleUname,
+			&r.ConsolePass,
+			&r.LocX,
+			&r.LocY,
+			&r.ExternalAddress,
+			&r.SlaveAddress,
+			&r.IsRunning,
+			&r.EstateName,
+		)
+		if err != nil {
+			rows.Close()
+			return nil, err
+		}
+		regions = append(regions, r)
+	}
+	return regions, nil
+}
+
+// GetRegionsOnHost retrieves all region records for a specified host
+func (db regionDatabase) GetRegionsOnHost(host mgm.Host) ([]mgm.Region, error) {
+	con, err := db.mysql.GetConnection()
+	if err != nil {
+		return nil, err
+	}
+	defer con.Close()
+
+	rows, err := con.Query(
+		"Select uuid, name, size, httpPort, consolePort, consoleUname, consolePass, locX, locY, externalAddress, slaveAddress, isRunning from regions "+
+			"where slaveAddress=?", host.Address)
+	defer rows.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	var regions []mgm.Region
+	for rows.Next() {
+		r := mgm.Region{}
+		err = rows.Scan(
+			&r.UUID,
+			&r.Name,
+			&r.Size,
+			&r.HTTPPort,
+			&r.ConsolePort,
+			&r.ConsoleUname,
+			&r.ConsolePass,
+			&r.LocX,
+			&r.LocY,
+			&r.ExternalAddress,
+			&r.SlaveAddress,
+			&r.IsRunning,
+		)
+		if err != nil {
+			rows.Close()
+			return nil, err
+		}
+		regions = append(regions, r)
+	}
+	return regions, nil
+}
+
+// GetRegionByID retrieves a single region that matches the id given
+func (db regionDatabase) GetRegionByID(id uuid.UUID) (mgm.Region, error) {
+	r := mgm.Region{}
+	con, err := db.mysql.GetConnection()
+	if err != nil {
+		return r, err
+	}
+	defer con.Close()
+
+	err = con.QueryRow(
+		"Select uuid, name, size, httpPort, consolePort, consoleUname, consolePass, locX, locY, externalAddress, slaveAddress, isRunning from regions "+
+			"where uuid=?", id.String()).Scan(
+		&r.UUID,
+		&r.Name,
+		&r.Size,
+		&r.HTTPPort,
+		&r.ConsolePort,
+		&r.ConsoleUname,
+		&r.ConsolePass,
+		&r.LocX,
+		&r.LocY,
+		&r.ExternalAddress,
+		&r.SlaveAddress,
+		&r.IsRunning,
+	)
+	if err != nil {
+		return r, err
+	}
+	if id != r.UUID {
+		return r, errors.New("Region Not Found")
+	}
+	return r, nil
+}
+
+// GetRegions gets all region records from the database
+func (db regionDatabase) GetRegions() ([]mgm.Region, error) {
+	con, err := db.mysql.GetConnection()
+	if err != nil {
+		return nil, err
+	}
+	defer con.Close()
+
+	rows, err := con.Query(
+		"Select uuid, name, size, httpPort, consolePort, consoleUname, consolePass, locX, locY, externalAddress, slaveAddress, isRunning, EstateName from regions, estate_map, estate_settings " +
+			"where estate_map.RegionID = regions.uuid AND estate_map.EstateID = estate_settings.EstateID")
+	defer rows.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	var regions []mgm.Region
+	for rows.Next() {
+		r := mgm.Region{}
+		err = rows.Scan(
+			&r.UUID,
+			&r.Name,
+			&r.Size,
+			&r.HTTPPort,
+			&r.ConsolePort,
+			&r.ConsoleUname,
+			&r.ConsolePass,
+			&r.LocX,
+			&r.LocY,
+			&r.ExternalAddress,
+			&r.SlaveAddress,
+			&r.IsRunning,
+			&r.EstateName,
+		)
+		if err != nil {
+			rows.Close()
+			return nil, err
+		}
+		regions = append(regions, r)
+	}
+	return regions, nil
 }
 
 func (db regionDatabase) GetDefaultConfigs() ([]mgm.ConfigOption, error) {
